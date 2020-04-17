455c6e38939bdce52d519d31f55aa41368ce151a
wang <wang1386528@gmail.com>
2020-04-17 16:56:51 +0800

    thread pool

--- juc/src/main/java/SingleThreadPoolTest.java
+++ juc/src/main/java/SingleThreadPoolTest.java
@@ -3,6 +3,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wanghuan
@@ -23,6 +28,13 @@ public class SingleThreadPoolTest {
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     ExecutorService executorService = Executors.newCachedThreadPool();
+    new ThreadPoolExecutor(2, // 核心线程数
+        10, // 最大线程数
+        60L, // 空闲线程等待新任务最长时间
+        TimeUnit.SECONDS, // 空闲线程等待新任务最长时间单位
+        new LinkedBlockingQueue<>(2), // 存放任务的队列
+        Executors.defaultThreadFactory(), // 线程工厂
+        new AbortPolicy()); // 拒绝策略
     for (int i = 0; i < myMission.length; ) {
       int[] ints = Arrays.copyOfRange(myMission, i, i += 50);
       Future<?> submit = executorService.submit(new MyTask(ints));
